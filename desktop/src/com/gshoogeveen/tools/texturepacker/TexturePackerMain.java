f6eb6ffd3f6d00e20c6ca9960735f14f4ec94bd1
GSamuel <gideon_hoog@hotmail.com>
2015-03-29 21:39:04 +0200

    zzz

---
+++ desktop/src/com/gshoogeveen/tools/texturepacker/TexturePackerMain.java
@@ -0,0 +1,13 @@
+package com.gshoogeveen.tools.texturepacker;
+
+import com.badlogic.gdx.tools.texturepacker.TexturePacker;
+
+public class TexturePackerMain
+{
+
+	public static void main(String[] args) throws Exception
+	{
+		TexturePacker.process("", "", "texturePacker.png");	
+	}
+
+}
